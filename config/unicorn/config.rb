d0b9a39a8fa0c31e485e492b2c022b0fb9e3b288
Danial Pearce <git@tigris.id.au>
2011-04-08 22:53:35 +1000

    Fix ports

--- config/unicorn/config.rb
+++ config/unicorn/config.rb
@@ -5,7 +5,7 @@ UNICORN_CONFIG = {
     pidfile: '/tmp/unicorn-battle.pid',
   },
   production: {
-    port:    [ 5001, 5002, 5003, 5004 ],
+    port:    [ 6001, 6002, 6003, 6004 ],
     pidfile: '/tmp/unicorn-battle.pid',
     stderr:  '/tmp/unicorn-battle.stderr.log',
     stdout:  '/tmp/unicorn-battle.stdout.log',
